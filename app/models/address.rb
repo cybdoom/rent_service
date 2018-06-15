898538dc16b45269a71627bb6b5039e3f7c7ec6f
cybdoom <cybdoom@gmail.com>
2018-06-15 13:23:51 +0300

    Implement directions

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,12 @@
 class Address
   include GoogleMapsApiClient
 
+  PLACES = [
+    '50.450015,30.523648',
+    '50.406669,30.519762',
+    '50.456183,30.364824'
+  ]
+
   def initialize(full_name)
     @full_name = full_name
   end
@@ -13,11 +19,15 @@ class Address
     {address: @full_name}.to_query
   end
 
+  def self.to_url_param(full_name)
+    URI.encode(full_name)
+  end
+
   private
 
   def load_info
     @coordinates ||= get_coordinates
-    @info = {location_map: location_map_url, coordinates: @coordinates}
+    @info = {location_map: location_map_url, coordinates: @coordinates, directions: directions(PLACES)}
   end
 
 end
--- app/models/concerns/google_maps_api_client.rb
+++ app/models/concerns/google_maps_api_client.rb
@@ -12,11 +12,25 @@ module GoogleMapsApiClient
   end
 
   def location_map_url
-    "#{GOOGLE_MAPS_API_ENDPOINT}/staticmap?key=#{app_key}&#{{center: @full_name}.to_query}&size=#{STATIC_MAP_SIZE}"
+    "#{GOOGLE_MAPS_API_ENDPOINT}/staticmap?key=#{app_key}&#{{center: @full_name}.to_query}&size=#{STATIC_MAP_SIZE}&zoom=15&markers=size:mid%7C#{Address.to_url_param(@full_name)}"
   end
 
   def app_key
     'AIzaSyCLyopNQLnHRYPFx9RlE55xdtNiRRTmHME'
+  end
+
+  def directions(places)
+    result = {}
+    places.each do |place|
+      url = "#{GOOGLE_MAPS_API_ENDPOINT}/directions/json?key=#{app_key}&origin=#{Address.to_url_param(@full_name)}&mode=walking&destination=#{place}"
+      response = JSON.parse(RestClient.get url)
+      result[place] = begin
+        response['routes'].first['legs'].first['distance']['text']
+      rescue nil
+      end
+    end
+
+    result
   end
 
 end
